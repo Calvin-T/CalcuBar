3c021c81ccdf69331519113ae5597891d59a8387
Calvin-T <tang.calvin1@gmail.com>
2019-01-19 23:25:12 -0500

    Initial Commit

---
+++ CalcuBar/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  CalcuBar
+//
+//  Created by Calvin Tang on 1/19/19.
+//  Copyright © 2019 Calvin Tang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
